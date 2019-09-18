66111961dd2ab3c0bc4a4c9f674028a68e4760e1
Yamagn <syankenpon@gmail.com>
2019-09-18 23:31:54 +0900

    Initial Commit

---
+++ bottakuri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bottakuri
+//
+//  Created by ymgn on 2019/09/18.
+//  Copyright © 2019 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
