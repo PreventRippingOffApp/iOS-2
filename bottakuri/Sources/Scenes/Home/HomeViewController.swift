a066cfb5efa21847a78e9d389d6ccb0583eca3e4
Yamagn <syankenpon@gmail.com>
2019-09-19 18:29:15 +0900

    Home画面の作成

--- bottakuri/Sources/Scenes/Home/HomeViewController.swift
+++ bottakuri/Sources/Scenes/Home/HomeViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton! {
+        didSet {
+            recordButton.layer.cornerRadius = 100
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showMap(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func emergency(_ sender: Any) {
+        
+    }
 }
