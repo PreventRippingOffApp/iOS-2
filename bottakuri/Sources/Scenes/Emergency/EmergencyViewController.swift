07021aa7f9c2c786a6a846d251ec27e0da4c741c
Yamagn <syankenpon@gmail.com>
2019-09-20 10:26:58 +0900

    通報画面の作成

--- bottakuri/Sources/Scenes/Emergency/EmergencyViewController.swift
+++ bottakuri/Sources/Scenes/Emergency/EmergencyViewController.swift
@@ -15,16 +15,7 @@ class EmergencyViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func emergencyCall(_ sender: Any) {
     }
-    */
-
+    
 }
