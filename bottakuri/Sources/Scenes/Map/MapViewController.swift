414f8c198d0d6a20bb1079c5b76ea26afa3e7347
Yamagn <syankenpon@gmail.com>
2019-12-26 12:28:19 +0900

    録音機能の移植

--- bottakuri/Sources/Scenes/Map/MapViewController.swift
+++ bottakuri/Sources/Scenes/Map/MapViewController.swift
@@ -36,43 +36,60 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     let userDefaults = UserDefaults.standard
     var isReadForReport: Bool = false
     
+    // Speech
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+    private var audioEngine = AVAudioEngine()
+    private var inputText: String = ""
+    
     @IBOutlet weak var whiteView: UIView! {
         didSet {
             whiteView.layer.cornerRadius = 20
             whiteView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-            whiteView.frame = CGRect(x:0,y:4*self.view.bounds.maxY/5,width:self.view.bounds.maxX,height:self.view.bounds.maxY/5)
         }
     }
     @IBOutlet weak var recordButton: UIButton! {
         didSet {
-            recordButton.frame.size = CGSize(width: self.view.bounds.maxX/3, height: self.view.bounds.maxY/15)
             recordButton.layer.cornerRadius = 20
             recordButton.backgroundColor = UIColor(
                 red:1.0, green: 0.0, blue: 0.0, alpha: 0.7)
             recordButton.setTitle("🐱", for: .normal)
-            recordButton.layer.position = CGPoint(x: self.view.bounds.midX, y:self.view.bounds.maxY/100)
         }
     }
     @IBOutlet weak var settingButton: UIButton! {
         didSet {
-            settingButton.frame.size = CGSize(width: self.view.bounds.maxX/3, height: self.view.bounds.maxY/15)
             settingButton.layer.cornerRadius = 3
             settingButton.layer.borderWidth = 1
             settingButton.layer.borderColor = UIColor.black.cgColor
             settingButton.setTitle("設定", for: .normal)
             settingButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
-            settingButton.layer.position = CGPoint(x: self.view.bounds.midX/2, y:self.view.bounds.maxY/10)
         }
     }
     @IBOutlet weak var recordListButton: UIButton! {
         didSet {
-            recordListButton.frame.size = CGSize(width: self.view.bounds.maxX/3, height: self.view.bounds.maxY/15)
             recordListButton.layer.cornerRadius = 3
             recordListButton.layer.borderWidth = 1
             recordListButton.layer.borderColor = UIColor.black.cgColor
             recordListButton.setTitle("ログ", for: .normal)
             recordListButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
-            recordListButton.layer.position = CGPoint(x: 3*self.view.bounds.midX/2, y:self.view.bounds.maxY/10)
+        }
+    }
+    
+    // MARK: - Life Cycle
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setUI()
+        SFSpeechRecognizer.requestAuthorization { (status) in
+            OperationQueue.main.addOperation {
+                        switch status {
+                        case .authorized:   // 許可OK
+                            self.recordButton.isEnabled = true
+                        default:
+                            self.recordButton.isEnabled = false
+                }
+            }
         }
     }
     
@@ -82,6 +99,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         startUpdatingLocation()
         getLocations()
         setProgress()
+        guard let fileArray = self.userDefaults.array(forKey: "fileArray") else { return }
+        for file in fileArray {
+            let url = self.userDefaults.url(forKey: file as! String)
+            self.urlArray.append(url!)
+        }
     }
     
     // MARK: - Map
@@ -170,5 +192,151 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     func updateProgress(prgCnt: Int){
         let prgPercentage:Float = Float(prgCnt / 10)
         progressView.setProgress(prgPercentage, animated: true)
+    }
+    
+    // MARK: - UI Process
+    
+    @IBAction func record(_ sender: Any) {
+        if isRecording {
+            guard let audioRecorder = self.audioRecorder else { fatalError("レコーダが見つかりませんでした") }
+            audioRecorder.stop()
+            userDefaults.set(self.fileArray, forKey: "fileArray")
+            userDefaults.set(self.url, forKey: fileArray.last!)
+            self.recognitionTask?.cancel()
+            self.recognitionTask?.finish()
+            self.audioEngine.stop()
+            self.audioEngine.inputNode.removeTap(onBus: 0)
+            isRecording = false
+        } else {
+            let session = AVAudioSession.sharedInstance()
+                       
+            do {
+                try session.setCategory(AVAudioSession.Category.playAndRecord)
+            } catch  {
+               fatalError("category err")
+            }
+
+            do {
+               try session.setActive(true)
+            } catch {
+               fatalError("session activate err")
+            }
+
+            let settings = [
+               AVFormatIDKey: Int(kAudioFormatLinearPCM),
+               AVSampleRateKey: 44100,
+               AVNumberOfChannelsKey: 1,
+               AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            do {
+                self.audioRecorder = try AVAudioRecorder(url: getURL(), settings: settings)
+            } catch {
+                self.audioRecorder = nil
+            }
+            guard let audioRecorder = self.audioRecorder else { fatalError("recorder生成エラー") }
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            try! startRecording()
+            isRecording = true
+        }
+    }
+    
+    @IBAction func showSetting(_ sender: Any) {
+        // TODO: Settignページを作る
+    }
+    
+    @IBAction func showLog(_ sender: Any) {
+        let recordListStoryBoard: UIStoryboard = UIStoryboard(name: "RecordList", bundle: nil)
+        let recordListViewController: RecordListViewController = recordListStoryBoard.instantiateViewController(withIdentifier: "RecordList") as! RecordListViewController
+        self.navigationController?.pushViewController(recordListViewController, animated: true)
+    }
+    
+    // MARK: - Record
+    private func startRecording() throws {
+        self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        self.audioEngine = AVAudioEngine()
+        let recordingFormat = audioEngine.inputNode.outputFormat(forBus: 0)
+        audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest.append(buffer)
+        }
+        try audioEngine.start()
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: self.recognitionRequest) { (result, error) in
+            if let result = result {
+                print(result.bestTranscription.formattedString)
+                self.inputText = result.bestTranscription.formattedString
+                
+                if self.inputText == "殺すぞ" {
+                    let alert = UIAlertController(title: nil, message: "通報の準備が整いました", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: { (_) in
+                        self.dismiss(animated: true, completion: nil)
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                    self.isReadForReport = true
+                }
+            }
+            
+            if error != nil {
+                print("fails with error: \(error!.localizedDescription)")
+            }
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0, execute: {
+            self.recognitionTask?.cancel()
+            self.recognitionTask?.finish()
+            self.audioEngine.stop()
+            print("RESTART")
+            if !self.isReadForReport || self.isRecording {
+                try! self.startRecording()
+            }
+        })
+    }
+    
+    func setVolumeView() {
+        let volumeView = MPVolumeView(frame: CGRect(origin:CGPoint(x:/*-3000*/ 0, y:0), size:CGSize.zero))
+        self.view.addSubview(volumeView)
+        NotificationCenter.default.addObserver(self, selector: #selector(HomeViewController.volumeChanged(notification:)), name:
+        NSNotification.Name("AVSystemController_SystemVolumeDidChangeNotification"), object: nil)
+    }
+    
+    @objc func volumeChanged(notification: NSNotification) {
+        if self.isReadForReport {
+            if let userInfo = notification.userInfo {
+                if let volumeChangeType = userInfo["AVSystemController_AudioVolumeChangeReasonNotificationParameter"] as? String {
+                    if volumeChangeType == "ExplicitVolumeChange" {
+                        let alert = UIAlertController(title: nil, message: "通報しました", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction.init(title: "OK", style: .default, handler: { (_) in
+                            self.dismiss(animated: true, completion: nil)
+                        }))
+                        present(alert, animated: true, completion: nil)
+                        self.isReadForReport = false
+                    }
+                }
+            }
+        }
+    }
+    
+    func getURL() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docsDirect = paths[0]
+        let f = DateFormatter()
+        f.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss"
+        f.locale = Locale(identifier: "ja_JP")
+        let now = f.string(from: Date())
+        self.url = docsDirect.appendingPathComponent(now)
+        guard let url = self.url else { fatalError("urlが取得できませんでした") }
+        self.fileArray.append(now)
+        self.urlArray.append(url)
+        return url
+    }
+    
+    // MARK: - UI Setting
+    func setUI() {
+        recordButton.frame.size = CGSize(width: self.view.bounds.maxX/3, height: self.view.bounds.maxY/15)
+        settingButton.frame.size = CGSize(width: self.view.bounds.maxX/3, height: self.view.bounds.maxY/15)
+        recordListButton.frame.size = CGSize(width: self.view.bounds.maxX/3, height: self.view.bounds.maxY/15)
+        whiteView.frame = CGRect(x:0,y:4*self.view.bounds.maxY/5,width:self.view.bounds.maxX,height:self.view.bounds.maxY/5)
+        recordButton.layer.position = CGPoint(x: self.view.bounds.midX, y:self.view.bounds.maxY/100)
+        settingButton.layer.position = CGPoint(x: self.view.bounds.midX/2, y:self.view.bounds.maxY/10)
+        recordListButton.layer.position = CGPoint(x: 3*self.view.bounds.midX/2, y:self.view.bounds.maxY/10)
     }
 }
